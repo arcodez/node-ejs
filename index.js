76f1eae1d48e7062c9fd768e570127785e7d2715
arcodez <abelileitor123@gmail.com>
2021-08-12 15:19:10 +0200

    deleted template folder

--- index.js
+++ index.js
@@ -5,17 +5,9 @@ const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 
-//Routes
-app.get("/", (req, res) => {
-  res.render("pages/index");
-});
-
-app.get("/about", (req, res) => {
-  res.render("pages/about");
-});
-
+//Routes 
 //routes recomendado
-//app.use(require("./routes/index"));
+app.use(require("./routes/index"));
 
 //Se configura la ruta de la carpeta Publica
 app.use(express.static(__dirname + "/public"));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/about", (req, res) => {
-  res.render("pages/about");
-});
-
 router.get("/", (req, res) => {
   res.render("pages/index");
 });
+
+router.get("/portfolio-details", (req, res) => {
+  res.render("pages/portfolio-details");
+});
+  
+router.get("/blog", (req,res) => {
+  res.render("pages/blog")
+})
+
+
+router.get("/blog-single", (req,res) => {
+  res.render("pages/blog-single")
+})
+
+
 
 module.exports = router;
